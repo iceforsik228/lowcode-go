31b5623b63ef8cb801bab80d03364f66ca4754eb
Stanislav <s.chihichin737@gmail.com>
2023-02-21 20:17:17 +0300

    fixes

--- models/database.go
+++ models/database.go
@@ -3,10 +3,11 @@ package models
 import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"os"
 )
 
 func ConnectDB() *gorm.DB {
-	db, err := gorm.Open("postgres", "host=127.0.0.1 port=5432 user=postgres dbname=lowcode password=0000 sslmode=disable")
+	db, err := gorm.Open("postgres", "host=127.0.0.1 port=5432 user=postgres dbname=lowcode password="+os.Getenv("PGPASSWORD")+" sslmode=disable")
 
 	if err != nil {
 		panic("Не удалось подключиться к базе данных")
